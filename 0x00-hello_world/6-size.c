1fb7d844ec0cd79f7a7d45ece6447efa5544bb21
embula <9946166+embula@users.noreply.github.com>
2022-09-08 22:54:18 +0300

    Task 6. Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int z;
 	float n;
 	char m;
-
+	
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(m));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(x));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(y));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(z));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(n));
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(m));
 	return (0);
 }
