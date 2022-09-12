092ee902cbddb8c94490617d9be9c14c2a2d461d
embula <9946166+embula@users.noreply.github.com>
2022-09-12 20:17:38 +0300

    Task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 #include "stdio.h"
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		char _put = "_putchar";
+	char _put = "_putchar";
 
-			_putchar(_put);
+	_putchar(_put);
 
-				_putchar('\n');
+	_putchar('\n');
 
-					return (0);
+	return (0);
 }
