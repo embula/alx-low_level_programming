8e089e26698dcf8bdbd1012d2366559b2aeabddd
Everlyn Mbula <embula24@gmail.com>
2022-09-12 10:13:16 -0700

    Task 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		char _put = "_putchar";
+
+			_putchar(_put);
+
+				_putchar('\n');
+
+					return (0);
+}
