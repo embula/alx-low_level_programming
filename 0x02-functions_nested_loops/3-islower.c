97f25be43d3f52a19e579ec9cce7feeede2ea737
embula <9946166+embula@users.noreply.github.com>
2022-09-15 20:54:27 +0300

    Task 3. Is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * _islower - a function that checks for lowercase character
- *   * @c: single letter input
- *    * Return: 1 if int c is lowercase, 0 if otherwise
- *     */
+ * _islower - a function that checks for lowercase character
+ * @c: single letter input
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ */
 int _islower(int y)
 {
 	if (y >= 'a' && y <= 'z')
