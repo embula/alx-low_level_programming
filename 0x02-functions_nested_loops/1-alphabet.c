86de2fd18705c31f9e5a8a8540c72f46a9a6b324
embula <9946166+embula@users.noreply.github.com>
2022-09-15 20:39:22 +0300

    Task 1. Write a function that prints the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- *  * print_alphabet - a function that prints the alphabet, in lowercase
- *   *
- *    * Return: Always 0 (Succes)
- *     */
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
 void print_alphabet(void)
 {
-		int n;
+	int i;
 
-			for (n = 'a'; n <= 'z'; n++)
-					{
-								_putchar(n);
-									}
-				_putchar('\n');
-}
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	}
