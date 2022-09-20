8b105cc4f9c87259a32de3d41a6b37ee003ee6f6
embula <9946166+embula@users.noreply.github.com>
2022-09-21 00:03:23 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,15 +8,15 @@
  */
 void puts2(char *str)
 {
-    int len = 0, i = 0;
+	int len = 0, i = 0;
 
-    while (str[len] != '\0')
-      len++;
+	while (str[len] != '\0')
+		len++;
 
-    len -= 1;
+	len -= 1;
 
-    for (; i <= len; i += 2)
-      _putchar(str[i]);
+	for (; i <= len; i += 2)
+		_putchar(str[i]);
 
-    _putchar('\n');
+	_putchar('\n');
 }
