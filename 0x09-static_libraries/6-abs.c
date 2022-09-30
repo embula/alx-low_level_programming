acbe943e7db521cd9884e49d8e3fc4e306b243a4
embula <embula24@gmail.com>
2022-09-30 20:07:06 +0300

    feat: Add functions

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *_abs - Check Holberton
+ *_abs - Check ALX
  * @r: An integre input
  * Description: This function returns absolute value of a number
  * Return: Absolut value of number r
