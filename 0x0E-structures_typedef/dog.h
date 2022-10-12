9ffdcdeb774d6c5bbdea1bdcbfb1b09e70d87716
embula <9946166+embula@users.noreply.github.com>
2022-10-12 21:35:06 +0300

    Task 0. Define a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,22 @@
-#include <stdio.h>
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Define a new type struct dog with the following elements:
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: data structure for dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
